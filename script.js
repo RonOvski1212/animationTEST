398cbfcbd53a43a43cf5fbcfed745e3d9e04b3c9
RonOvski1212 <245785gio@gmail.com>
2023-07-18 23:07:24 +0400

    setTimeout and recurse

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector(".btn");
+let timerId,
+  i = 0;
+
+// btn.addEventListener("click", () => {
+//   timerId = setInterval(logger, 500);
+// });
+
+// function logger() {
+//   if (i === 3) {
+//     clearInterval(timerId);
+//   }
+//   console.log("text");
+//   i++;
+// }
+
+// let id = setTimeout(function log() {
+//   console.log("Hi");
+//   id = setTimeout(log, 1000);
+// }, 5000);
